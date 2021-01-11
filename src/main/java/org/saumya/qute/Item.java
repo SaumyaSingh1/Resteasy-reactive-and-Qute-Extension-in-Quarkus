925971c34ab7c3e1a5119695238be188eb9392d0
SaumyaSingh1 <ssaumyas100@gmail.com>
2021-01-11 11:39:03 +0530

    Adding Endpoint that return Qute Template

--- src/main/java/org/saumya/qute/Item.java
+++ src/main/java/org/saumya/qute/Item.java
@@ -1,6 +1,12 @@
 package org.saumya.qute;
+import java.math.BigDecimal;
 
 public class Item {
     public String name;
-    public Float price;
+    public BigDecimal price;
+
+    public Item(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
 }
---
+++ src/main/java/org/saumya/qute/ItemList.java
@@ -0,0 +1,41 @@
+package org.saumya.qute;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateExtension;
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.qute.api.CheckedTemplate;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/items")
+public class ItemList {
+
+    @Inject
+    Template items;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get(){
+        List<Item> list= new ArrayList<>();
+        list.add(new Item("Apple", new BigDecimal(80)));
+        list.add(new Item("Mango", new BigDecimal(88)));
+        list.add(new Item("Dairy Milk", new BigDecimal(770)));
+        list.add(new Item("Grapes", new BigDecimal(76)));
+        return items.data("items", list);
+    }
+
+    //Template extensions allow to add new methods to the POJO class that will be
+    //available from Template
+    @TemplateExtension
+    public static BigDecimal discountedPrice(Item item){
+        return item.price.multiply(new BigDecimal("0.9"));
+    }
+}
